308aaa85521bed6f65982fdfc65d20f949d77488
andrew21b <32705107+andrew21b@users.noreply.github.com>
2018-02-13 19:12:24 +0000

    Fixed untracked files

--- bin/StartClient.bat
+++
@@ -1,5 +0,0 @@
-TITLE Client
-javac -cp ct414.jar client\GetAssessment.java
-java -cp C:\Users\Andrew\Documents\College\java\ct414\src;C:\Users\Andrew\Documents\College\java\ct414\src\ct414.jar -Djava.rmi.server.codebase=file:C:\Users\Andrew\Documents\College\java\ct414\src\ct414.jar -Djava.rmi.server.hostname=localhost -Djava.security.policy=client.policy client.GetAssessment 
-pause
-
