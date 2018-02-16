64246f3b0644306b645f9bd9959508c4bdb17c41
andrew21b <32705107+andrew21b@users.noreply.github.com>
2018-02-16 18:09:55 +0000

    Fixed read in for question, answers and user selected answers

--- src/client/radioOptionAssignment.java
+++ src/client/radioOptionAssignment.java
@@ -3,6 +3,8 @@ package client;
 import java.awt.*;       // Using layouts
 import java.awt.event.*; // Using AWT event classes and listener interfaces
 import javax.swing.*;    // Using Swing components and containers
+import server.MCQ;
+import server.MCQAssessment;
 
 import com.sun.corba.se.impl.ior.NewObjectKeyTemplateBase;
 
@@ -14,6 +16,7 @@ import server.Question;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Enumeration;
 import java.util.List;
 
 public class radioOptionAssignment extends JFrame {
@@ -31,7 +34,9 @@ public class radioOptionAssignment extends JFrame {
 	//private ArrayList<Answer> answers = new ArrayList<Answer>();
 	private ArrayList<Question> questions = new ArrayList<Question>();
 	public ArrayList<JRadioButton> buttons = new ArrayList<JRadioButton>();
-	private ArrayList<int[]> completedAnswers = new ArrayList<int[]>();
+	//private ArrayList<String> completedAnswers = new ArrayList<String>();
+	private ArrayList<Integer> l= new ArrayList<Integer>();
+	private ArrayList<ButtonGroup> groups = new ArrayList<ButtonGroup>();
 	
 	//public Assesment(int i, List<String> questionList) {
 	//public radioOptionAssignment(ArrayList<String> assignmentQuestions, int userInfo) {
@@ -40,38 +45,38 @@ public class radioOptionAssignment extends JFrame {
 		
 	assessment = assignment;
 	
-	questions = assignment.getQuestions();
+	questions = assessment.getQuestions();
 	
-	ArrayList<Question> x = assessment.getQuestions();
+	System.out.println("num questions: "+assessment.getQuestions().size());
 	
-	String[] y = x.get(1).getAnswerOptions();
+	for(int k = 0; k<questions.size();k++){
+		l.add(questions.get(k).getAnswerOptions().length);
+	}	
 	
-	System.out.println(y[1]);
-	
-	int[] answers = {2,2,2,2,2,2,2};
-	
-	for (int i = 0;i<answers.length; i++){
+	for (int i = 0;i<l.size(); i++){
 		
-		if (answers[i] == 2){
+		if (l.get(i) == 2){
 			twos ++;
 		}
-		else if (answers[i]==3){
+		else if (l.get(i)==3){			//this is used to determine how many lines need to be dynamically created on the screen
 			threes ++;
 		}
-		else if (answers[i] == 4){
+		else if (l.get(i) == 4){
 			fours ++;
 		}
 	}
-
 	
-	int rows = twos+(threes*2)+(fours*2)+2;
+	
+	
+	
+	int rows = twos+(threes*2)+(fours*2)+2;  //for 3 and 4 answer questions there need to be 2 lines to fit the answers.
 	
 	Container cp = getContentPane();
     cp.setLayout(new GridLayout(rows, 3, 5, 5));  // The content-pane sets its layout
     
     
     cp.add(new JLabel("Question"));
-    cp.add(new JLabel(" "));
+    cp.add(new JLabel(" "));			//formating the screen
     cp.add(new JLabel(" "));
     
     //String a = questions.get(0);
@@ -79,61 +84,77 @@ public class radioOptionAssignment extends JFrame {
     //cp.add(new JLabel(newQuestion);
     for (int j = 0;j<assessment.getQuestions().size(); j++){
     	
-    	if(answers[j] == 2){
-	    	cp.add(new JLabel("Test"));
-	    	radioAnswer1 = new JRadioButton("True");
-	        radioAnswer2 = new JRadioButton("Flase");
-	        cp.add(radioAnswer1);
-	        cp.add(radioAnswer2);
-	        buttons.add(radioAnswer1);
-	        buttons.add(radioAnswer2);
-	        
-	        group = new ButtonGroup();
-	        group.add(radioAnswer1);
-	        group.add(radioAnswer2); 
-    	}
-    	else if (answers[j] == 3){
-    		cp.add(new JLabel("Test"));
-	    	radioAnswer1 = new JRadioButton("1");
-	    	radioAnswer2 = new JRadioButton("2");
-	    	radioAnswer3 = new JRadioButton("3");
-	        cp.add(radioAnswer1);
-	        cp.add(radioAnswer2);
-	        cp.add(new JLabel(" "));
-	        cp.add(radioAnswer3);
-	        cp.add(new JLabel(" "));
-	        buttons.add(radioAnswer1);
-	        buttons.add(radioAnswer2);
-	        buttons.add(radioAnswer3);
-	        buttons.add(radioAnswer4);
-	        
-	        group = new ButtonGroup();
-	        group.add(radioAnswer1);
-	        group.add(radioAnswer2);
-	        group.add(radioAnswer3);
-    	}
-    	else if (answers[j] == 4){
-    		cp.add(new JLabel("Test"));
-	    	radioAnswer1 = new JRadioButton("1");
-	    	radioAnswer2 = new JRadioButton("2");
-	    	radioAnswer3 = new JRadioButton("3");
-	    	radioAnswer4 = new JRadioButton("4");
-	        cp.add(radioAnswer1);
-	        cp.add(radioAnswer2);
-	        cp.add(new JLabel(" "));
-	        cp.add(radioAnswer3);
-	        cp.add(radioAnswer4);
-	        buttons.add(radioAnswer1);
-	        buttons.add(radioAnswer2);
-	        buttons.add(radioAnswer3);
-	        buttons.add(radioAnswer4);
-	        
-	        group = new ButtonGroup();
-	        group.add(radioAnswer1);
-	        group.add(radioAnswer2);
-	        group.add(radioAnswer3);
-	        group.add(radioAnswer4);
-    	}
+    	String[]options = questions.get(j).getAnswerOptions();
+    	
+    	//for (int k= 0; k < questions.get(j).getAnswerOptions().length;k++){
+	    	if(l.get(j)== 2){
+		    	cp.add(new JLabel(questions.get(j).getQuestionDetail()));
+		    	radioAnswer1 = new JRadioButton(options[0]);
+		    	radioAnswer1.setActionCommand("0");
+		        radioAnswer2 = new JRadioButton(options[1]);				//Loop for 2 answer questions
+		        radioAnswer2.setActionCommand("1");
+		        cp.add(radioAnswer1);
+		        cp.add(radioAnswer2);
+		        buttons.add(radioAnswer1);
+		        buttons.add(radioAnswer2);
+		        
+		        group = new ButtonGroup();
+		        group.add(radioAnswer1);
+		        group.add(radioAnswer2); 
+		        groups.add(group);
+	    	}
+	    	else if (l.get(j) == 3){
+	    		cp.add(new JLabel(questions.get(j).getQuestionDetail()));
+		    	radioAnswer1 = new JRadioButton(options[0]);
+		    	radioAnswer1.setActionCommand("0");
+		    	radioAnswer2 = new JRadioButton(options[1]);
+		    	radioAnswer2.setActionCommand("1");
+		    	radioAnswer3 = new JRadioButton(options[2]);
+		    	radioAnswer3.setActionCommand("2");
+		        cp.add(radioAnswer1);
+		        cp.add(radioAnswer2);							//loops for 3 answer questions
+		        cp.add(new JLabel(" "));
+		        cp.add(radioAnswer3);
+		        cp.add(new JLabel(" "));
+		        buttons.add(radioAnswer1);
+		        buttons.add(radioAnswer2);
+		        buttons.add(radioAnswer3);
+		        buttons.add(radioAnswer4);
+		        
+		        group = new ButtonGroup();
+		        group.add(radioAnswer1);
+		        group.add(radioAnswer2);
+		        group.add(radioAnswer3);
+		        groups.add(group);
+	    	}
+	    	else if (l.get(j) == 4){
+	    		cp.add(new JLabel(questions.get(j).getQuestionDetail()));
+		    	radioAnswer1 = new JRadioButton(options[0]);
+		    	radioAnswer1.setActionCommand("0");
+		    	radioAnswer2 = new JRadioButton(options[1]);
+		    	radioAnswer2.setActionCommand("1");
+		    	radioAnswer3 = new JRadioButton(options[2]);
+		    	radioAnswer3.setActionCommand("2");
+		    	radioAnswer4 = new JRadioButton(options[3]);
+		    	radioAnswer4.setActionCommand("3");				//loop for 4 answer questions
+		        cp.add(radioAnswer1);
+		        cp.add(radioAnswer2);
+		        cp.add(new JLabel(" "));
+		        cp.add(radioAnswer3);
+		        cp.add(radioAnswer4);
+		        buttons.add(radioAnswer1);
+		        buttons.add(radioAnswer2);
+		        buttons.add(radioAnswer3);
+		        buttons.add(radioAnswer4);
+		        
+		        group = new ButtonGroup();
+		        group.add(radioAnswer1);
+		        group.add(radioAnswer2);
+		        group.add(radioAnswer3);
+		        group.add(radioAnswer4);
+		        groups.add(group);
+	    	}
+    	//}
     	
     }
     
@@ -147,14 +168,24 @@ public class radioOptionAssignment extends JFrame {
     submitAnswers.addActionListener(new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent evt) {
-        	for (int i = 0; i<buttons.size();i++){
+        	String x = null;
+        	
+        	for (int i = 0; i<groups.size();i++){
+      		
+        			x = groups.get(i).getSelection().getActionCommand(); // reads in the ActionCommand for the button selected in int form for each question.
         		
-        		if (buttons.get(i).isSelected()){
-        			System.out.println(buttons.get(i).getLabel());
-        		}
+        			System.out.println(i+"    "+x);
+        			
+//I couldnt figure this out, sorry!!!        			//MCQAssessment.selectAnswer(i, Integer.parseInt(x)); //
+        			
+        			
+        			
+        			
+        		//}
         		
         	}
         }
+        
     });
 	
 	setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);  // Exit program if close-window button clicked
